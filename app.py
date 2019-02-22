ab8e32658a08682a476eb4c8dfcc2a92b27b9a8f
Irdi Caushi <irdicaushi@hotmail.com>
2019-02-22 17:18:24 +0200

    final push

--- app.py
+++ app.py
@@ -1,15 +1,21 @@
 from flask import Flask, render_template
 from web.public import course
 from api.item import student_user
-
+from extensions import db
 
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///test21.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=True
+
+db.init_app(app)
+
+# db.create_all()
+
+@app.before_first_request
+def create():
+    db.create_all()
 
 @app.route('/')
 def hello():
     return "Hello from Irdi"
-
-app.register_blueprint(course,url_prefix="/web/")
-
-app.register_blueprint(student_user,url_prefix="/v1/admin")
---
+++ models/users.py
@@ -0,0 +1,9 @@
+from extensions import db
+
+class UserModel(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, unique=True)
+    password = db.Column(db.String)
+
+    
